2f669fa96f9e208185a4ede5c32a73c5ba236280
dagmawim <dagmawi.mesfin@ymail.com>
2017-10-17 01:56:36 -0400

    fixed alu and alu control issue, added control comp

--- TermProject2/alu.vhd
+++ TermProject2/alu.vhd
@@ -5,53 +5,61 @@ use IEEE.NUMERIC_STD.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 -- generating ALU object
 entity ALU is
-	-- assigning ports for the ALU (input and output)
-	port( signal_1, signal_2: in std_logic_vector(31 downto 0);
-		operation : in std_logic_vector(3 downto 0); shift: in std_logic_vector(4 downto 0);
-		zero: out std_logic;
-		result: out std_logic_vector(31 downto 0));
-end entity;
+generic(n:natural:=32);
+port(a,b:in std_logic_vector(n-1 downto 0);
+	  Oper:in std_logic_vector(3 downto 0);
+	  Result:buffer std_logic_vector(n-1 downto 0);
+	  Zero,Overflow:buffer std_logic);
+ end ALU;
+
 
 -- generating an ALU's behavior
 architecture Behavioral of ALU is
 	--signal temp: std_logic_vector(32 downto 0);
-       signal sht: integer;
+       	--signal sht: integer;
+	signal shift:std_logic_vector(4 downto 0);
+
 	begin
-	process(signal_1,signal_2,operation,shift)
+	process(a,b,Oper,shift)
         
 	variable var1,var2,var3:integer;
 	begin
 		zero <= '0';
-		case operation is
+		shift <= b(10 downto 6);
+		-- condition on operation
+		case Oper is
+			when "0000" =>
+				result <=a and b;
+				--var1:= CONV_INTEGER(a);
+				--var2:= CONV_INTEGER(a);
+				--var3:= var1 * var2;
+				--result <= std_logic_vector(to_unsigned(var3,32));
+				zero <= '0';
+			when "0001" =>
+				result <= a or b;
+				zero <= '0';
 			when "0010" =>
-				result <= std_logic_vector((unsigned(signal_1)+unsigned(signal_2)));
+				result <= std_logic_vector((unsigned(a)+unsigned(b)));
 				zero <= '0';
 			when "0110" =>
-				result <= std_logic_vector(unsigned(signal_1) - unsigned(signal_2));
-				if std_logic_vector(unsigned(signal_1) - unsigned(signal_2)) = "00000000000000000000000000000000" then
+				result <= std_logic_vector(unsigned(a) - unsigned(b));
+				if std_logic_vector(unsigned(a) - unsigned(b)) = "00000000000000000000000000000000" then
 					zero <= '1';
 				end if;
-			when "0001" =>
-				result <= signal_1 or signal_2;
-				zero <= '0';
-			when "0000" =>
-				--result <=signal_1 and signal_2;
-				var1:= CONV_INTEGER(signal_1);
-				var2:= CONV_INTEGER(signal_1);
-				var3:= var1 * var2;
-				result <= std_logic_vector(to_unsigned(var3,32));
-				zero <= '0';
 			when "0111" =>
-				if signal_1 < signal_2 then
+				if a < b then
 					result <= x"00000001";
 				end if;
-				if signal_1 > signal_2 then
+				if a > b then
 					result <= x"00000000";
 				end if;
 				zero <= '0';
 			when "1111" =>
-				result <= std_logic_vector(unsigned(signal_2) sll to_integer(unsigned(shift)));
-				sht <= to_integer(unsigned(shift));
+				result <= std_logic_vector(unsigned(a) sll to_integer(unsigned(shift)));
+				--sht <= to_integer(unsigned(shift));
+			--when NOR condition here
+			when "1100" =>
+				result <= not(a or b);
 			when others  =>
 				result <= x"00000000";	
 				zero <= '0';		
